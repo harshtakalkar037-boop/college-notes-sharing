5d5deaa68aecb4024416d8289707012625eb3d02
Harsh Takalkar <harshtakalkar037@gmail.com>
2025-08-27 16:18:19 +0530

    Update frontend to use deployed backend

--- backend/server.js
+++ backend/server.js
@@ -9,6 +9,8 @@ const app = express();
 // Middleware
 app.use(cors());
 app.use(express.json());
+app.use(express.static("public"));
+
 app.use("/uploads", express.static("uploads"));
 
 // Multer storage configuration
